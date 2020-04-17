1d124e67bfd083cd4c2c110103b3255b72449977
shivam singh <shivama5tronot@gmail.com>
2020-04-17 14:10:33 +0530

    connected to mongodb cloud atlas

--- config/database.js
+++ config/database.js
@@ -1,8 +1,9 @@
 const mongoose=require('mongoose');
-const connectDatabase=()=>{mongoose.connect(process.env.DB_LOCAL_URI,{
+
+const connectDatabase=()=>{mongoose.connect(process.env.DB_URI ,{
     useNewUrlParser : true,
     useUnifiedTopology : true,
-    useCreateIndex : true
+    useCreateIndex :true
 }).then(con=>{
     console.log(`mongodb database connected with host : ${con.connection.host} `)
 })};
